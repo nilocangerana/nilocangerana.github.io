b646fc564a12d901bffbd2fd802a77f62a3c2595
Nilo Cangerana <40279655+nilocangerana@users.noreply.github.com>
2026-05-04 01:12:05 -0300

    build

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 export default defineConfig({
   title: 'Nilo Cangerana - Portfolio',
+  base: '/nilocangerana.github.io/',
   appearance: false,
 
   themeConfig: {
