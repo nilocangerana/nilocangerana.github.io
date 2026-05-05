2271bbfbed60f9f762a4edd6d1798408f87bdada
Nilo Cangerana <niloconrado2008@hotmail.com>
2026-05-05 18:54:55 -0300

    analytics

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,6 +3,22 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: 'Nilo Cangerana - Portfolio',
   appearance: false,
+  head: [
+    [
+      'script',
+      { async: '', src: 'https://www.googletagmanager.com/gtag/js?id=G-9RWLPHHB6B' }
+    ],
+    [
+      'script',
+      {},
+      `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-9RWLPHHB6B');
+      `
+    ]
+  ],
 
   themeConfig: {
     logo: undefined,
