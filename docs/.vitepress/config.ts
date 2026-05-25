d9fd23a229fe6c2e3f63d486b5c68ace7bf4af14
Nilo Cangerana <40279655+nilocangerana@users.noreply.github.com>
2026-05-25 19:53:49 -0300

    language settings
    +update version

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,16 +4,35 @@ export default defineConfig({
   lang: 'en-US',
   locales: {
     root: {
-      label: 'English',
+      label: '🇺🇸 English',
       lang: 'en-US',
+      title: 'Nilo Cangerana - Portfolio',
+      themeConfig: {
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'GameDev', link: '/gamedev' },
+          { text: 'FullStackDev', link: '/fullstack' },
+          { text: 'AI / Machine Learning', link: '/aiml' },
+          { text: 'Other', link: '/other' }
+        ]
+      }
     },
     pt: {
-      label: 'Português',
+      label: '🇧🇷 Português',
       lang: 'pt-BR',
+      title: 'Nilo Cangerana - Portfólio',
+      themeConfig: {
+        nav: [
+          { text: 'Início', link: '/pt/' },
+          { text: 'GameDev', link: '/pt/gamedev' },
+          { text: 'FullStackDev', link: '/pt/fullstack' },
+          { text: 'AI / Machine Learning', link: '/pt/aiml' },
+          { text: 'Outros', link: '/pt/other' }
+        ]
+      }
     }
   },
   
-  title: 'Nilo Cangerana - Portfolio',
   appearance: false,
   head: [
     [
@@ -35,13 +54,6 @@ export default defineConfig({
   themeConfig: {
     logo: undefined,
     outline: false,
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'GameDev', link: '/gamedev' },
-      { text: 'FullStackDev', link: '/fullstack' },
-      { text: 'AI / Machine Learning', link: '/aiml' },
-      { text: 'Other', link: '/other' }
-    ],
     sidebar: undefined
   },
 })
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,9 +1,9 @@
 import DefaultTheme from 'vitepress/theme'
-import { h } from 'vue'
+//import { h } from 'vue'
 import { useRoute } from 'vitepress'
 import Collapsible from './components/Collapsible.vue'
 //import ThemeToggle from './components/ThemeToggle.vue'
-import LangToggle from './components/LangToggle.vue'
+//import LangToggle from './components/LangToggle.vue'
 import './custom.css'
 
 export default {
@@ -11,16 +11,16 @@ export default {
   enhanceApp({ app }) {
     app.component('Collapsible', Collapsible)
     //app.component('ThemeToggle', ThemeToggle)
-    app.component('LangToggle', LangToggle)
+    //app.component('LangToggle', LangToggle)
   },
 
   //Toogle
-  Layout() {
-  return h(DefaultTheme.Layout, null, {
-    'nav-bar-content-before': () => h('div', { style: 'display:flex; gap:10px;' }, [
-      //h(ThemeToggle),
-      h(LangToggle)
-    ])
-  })
-}
+  /*Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'nav-bar-content-before': () => h('div', { style: 'display:flex; gap:10px;' }, [
+        //h(ThemeToggle),
+        h(LangToggle)
+      ])
+    })
+  }*/
 }
